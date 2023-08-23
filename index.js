3a1e42a30dc499c6ecf90ef43b0d2c04f9659042
vanp.uyn <vanphappi3.14@gmail.com>
2023-08-23 13:46:47 +0700

    up

---
+++ index.js
@@ -0,0 +1,8 @@
+//import { ethers } from 'ethers';
+const { MetaMaskSDK } = require('@metamask/sdk');
+
+document.getElementById("verify_button").onclick = function() {verify()};
+        
+function verify() {
+    alert('h')
+}
